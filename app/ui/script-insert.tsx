8580d224e7ae214ef9af736716fbb903ae4e6fea
hunterlan <konstantin.sharykin@outlook.com>
2023-12-04 12:34:05 +0100

    Update insert link to button
    usePathname hooks is right way to get host path

--- app/ui/script-insert.tsx
+++ app/ui/script-insert.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import Script from "next/script";
+import {usePathname} from "next/navigation";
 
 export default function ScriptInsert({page} : {page: string}) {
-    const urlToInsert = window.location.origin + '/news?language=en&page=' + page;
+    const pathname = usePathname()
+    const hostname = pathname!.split('/')[0];
+    const urlToInsert = hostname + '/news?language=en&page=' + page;
     const contentScript = 'document.getElementById("next-page-link").href = "' + urlToInsert + '"';
     return (
         <Script id='insert-next-page-link'>
